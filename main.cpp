63dbc91bd6a8ee3a77d3b0c021a336f8f61ca62b
faishal <faaizfaishal@gmail.com>
2023-10-16 13:51:07 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "-----------------------------------------------" << endl;
+    cout << "Nama : Muhammad Faishal Faa'iz" << endl << endl;
+    cout << "NPM : 23106311701" << endl << endl;
+    cout << "Kelas : B - Informatika" << endl << endl;
+    cout << "Cita-cita : Freelancer professional" << endl << endl;
+    cout << "Motivasi masuk Informatika : Sering main gadget" << endl;
+    cout << "-----------------------------------------------" << endl;
+    return 0;
+}
